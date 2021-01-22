e8dab38b5c5c2cb1e14f047d7cbbabcdbf94e79b
Prathamesh Sai <prathameshsaisankar@Gmail.com>
2021-01-22 21:10:42 +0000

    added time and space complexity

--- TwoSum/twoSum.java
+++ TwoSum/twoSum.java
@@ -3,6 +3,14 @@ import java.util.Map;
 
 class twoSum {
 	
+//Complexity Analysis:
+
+//Time complexity : O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1) time.
+
+//Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, which stores at most n elements.	
+	
+	
+	
 //One-pass Hash Table
 public int[] twoSum(int[] nums, int target) {
 	
